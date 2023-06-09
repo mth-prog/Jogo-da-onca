c85765f4cbaf71fd6411f4a98904866ef9860a7f
Mateus dos santos <53189625+mth-prog@users.noreply.github.com>
2023-06-09 18:58:42 -0300

    Update s3Connection.js

--- Api/src/models/s3Connection.js
+++ Api/src/models/s3Connection.js
@@ -1,14 +1,20 @@
 require('dotenv').config();
 const { S3Client } = require('@aws-sdk/client-s3');
 
+// const s3 = new S3Client({
+//     credentials: {
+//         accessKeyId: process.env.ACCESS_KEY,
+//         secretAccessKey: process.env.SECRET_ACCESS_KEY
+//     }, 
+//     region: process.env.BUCKET_REGION
+// });
+
 const s3 = new S3Client({
     credentials: {
-        accessKeyId: process.env.ACCESS_KEY,
-        secretAccessKey: process.env.SECRET_ACCESS_KEY
+        accessKeyId: 'AKIA3FXBHN32WXVSHSD5',
+        secretAccessKey: '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'
     }, 
-    region: process.env.BUCKET_REGION
+    region: 'us-east-1'
 });
-
-
 
 module.exports = s3;
